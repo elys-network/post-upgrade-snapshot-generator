69bbfa7aa8d3f6b7e47ad62c49a28b3c0a2938c3
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-08-07 09:12:11 +0200

    fix: address prefix (#12)

--- cmd/createsecondvalidator/create-second-validator-cmd.go
+++ cmd/createsecondvalidator/create-second-validator-cmd.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/spf13/cobra"
 
+	elysdcmd "github.com/elys-network/elys/cmd/elysd/cmd"
 	"github.com/elys-network/post-upgrade-snapshot-generator/flags"
 	"github.com/elys-network/post-upgrade-snapshot-generator/types"
 	"github.com/elys-network/post-upgrade-snapshot-generator/utils"
@@ -17,6 +18,9 @@ func CreateSecondValidatorCmd() *cobra.Command {
 		Short: "Create a second validator",
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
+			// set prefix
+			elysdcmd.InitSDKConfig()
+
 			// get args
 			oldBinaryUrl := args[0]
 			if oldBinaryUrl == "" {
