d40cecb6ce3cc6d7d3c7b242d9da22581989a3bf
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-11-05 19:08:04 +0100

    fix: feepool balances

--- utils/update-genesis.go
+++ utils/update-genesis.go
@@ -85,7 +85,7 @@ func UpdateGenesis(validatorBalance, homePath, genesisFilePath string) {
 	genesis.AppState.Distribution = genesisInit.AppState.Distribution
 
 	// temporary fix for distribution params
-	genesis.AppState.Distribution.FeePool.CommunityPool = sdk.NewDecCoins(sdk.NewDecCoin("ueden", sdk.NewInt(462213395436)), sdk.NewDecCoin("uedenb", sdk.NewInt(1540707087317)))
+	genesis.AppState.Distribution.FeePool.CommunityPool = sdk.NewDecCoins(sdk.NewDecCoin("ueden", sdk.NewInt(497204055696)), sdk.NewDecCoin("uedenb", sdk.NewInt(1657342689515)))
 
 	log.Printf("community pool: %v", genesis.AppState.Distribution.FeePool.CommunityPool)
 
