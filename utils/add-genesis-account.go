b430d230172eb843f1d2571fc5cfadcfec22569a
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-12-16 02:04:25 +0100

    feat: update validator balance handling and enhance genesis configuration
    - Upgrade elys-network/elys dependency to v1.0.0 for improved functionality.
    - Remove deprecated validator balance flags and replace them with a new flag for multiple validator balances.
    - Update command logic to handle the new validator balances format in both chain initialization and proposal submission commands.
    - Refactor genesis account addition and update functions to accommodate the new balance structure, ensuring accurate supply updates for validators.
    - Enhance asset profile and oracle configurations by adding support for WBTC and WETH, including their respective prices and decimals.

--- utils/add-genesis-account.go
+++ utils/add-genesis-account.go
@@ -3,19 +3,30 @@ package utils
 import (
 	"log"
 	"os/exec"
+	"strings"
 
 	"github.com/elys-network/post-upgrade-snapshot-generator/types"
 )
 
-func AddGenesisAccount(cmdPath, address, balance, homePath string) {
+func AddGenesisAccount(cmdPath, address, homePath string, balances []string) {
+	// Build the balance string
+	balanceStr := strings.Join(balances, ",")
+
 	// Command and arguments
-	args := []string{"genesis", "add-genesis-account", address, balance + "uelys," + balance + "ibc/F082B65C88E4B6D5EF1DB243CDA1D331D002759E938A0F5CD3FFDC5D53B3E349," + balance + "ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9", "--home", homePath}
+	args := []string{
+		"genesis",
+		"add-genesis-account",
+		address,
+		balanceStr,
+		"--home",
+		homePath,
+	}
 
 	// Execute the command
 	if err := exec.Command(cmdPath, args...).Run(); err != nil {
-		log.Fatalf(types.ColorRed+"Command execution failed: %v", err) // nolint: goconst
+		log.Fatalf(types.ColorRed+"Command execution failed: %v", err)
 	}
 
 	// If execution reaches here, the command was successful
-	log.Printf(types.ColorYellow+"add genesis account with address %s, balance: %s and home path %s successfully", address, balance, homePath)
+	log.Printf(types.ColorYellow+"add genesis account with address %s, balance: %s and home path %s successfully", address, balanceStr, homePath)
 }
